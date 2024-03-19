df31e528f37c7d9b741cf48b3c8aaaaee8a2e588
DemaNidal <khalilidema@gmail.com>
2024-03-19 15:35:41 +0200

    purchase

--- src/catalog-service/index.js
+++ src/catalog-service/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Define the path to the data file
-const dataFilePath = path.resolve(__dirname, 'C:\\Users\\maysh\\Desktop\\Dos\\project', 'BookData.txt');
+const dataFilePath = path.resolve(__dirname, 'C:\\DOS\\project', 'BookData.txt');
 
 
 
@@ -59,6 +59,47 @@ app.get('/info/:id', (req, res) => {
 });
 
 
+
+app.post('/purchase/:id', (req, res) => {
+  const id = req.params.id.trim();
+  const data = readDataFromFile();
+  console.log('Data from file:', data); 
+  const foundItemIndex = data.findIndex(row => {
+      console.log('Row ID:', row.id); 
+      console.log('Requested ID:', id); 
+      return row.id === String(id);
+  });
+  console.log('Searching for ID:', id); 
+  if (foundItemIndex !== -1) {
+    
+      const foundItem = data[foundItemIndex];
+    
+      if (foundItem.numberOfItems > 0) {
+        
+          foundItem.numberOfItems--;
+        
+          const updatedData = data.map((row, index) => {
+              if (index === foundItemIndex) {
+                  return foundItem;
+              } else {
+                  return row;
+              }
+          });
+          const newDataString = updatedData.map(row => Object.values(row).join(',')).join('\n');
+          fs.writeFileSync(dataFilePath, newDataString);
+          
+          res.json({ item: foundItem });
+      } else {
+        
+          console.log('Not enough items in stock for ID:', id);
+          res.status(400).json({ error: 'Not enough items in stock' });
+      }
+  } else {
+      
+      console.log('Item not found for ID:', id); 
+      res.status(404).json({ error: 'Item not found' });
+  }
+});
 
 
 
